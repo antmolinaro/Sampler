0edfab4180787e0f8230cb51dbbbd4f1a5242e1e
antmolinaro <ant.molinaro@gmail.com>
2017-05-03 22:27:07 -0400

    Corresponding measures on the bottom of page 51

--- ch5/p51-table5.1.r
+++ ch5/p51-table5.1.r
@@ -1,3 +1,14 @@
 # Create table 5.1 on page 51
 
 p51table5.1=read.csv("https://raw.githubusercontent.com/antmolinaro/Sampler/master/ch5/p51-table5.1.csv", header=T)
+
+# Additional statistics on p.51
+
+sum(p51table5.1$Actual)
+sum(p51table5.1$Reported)
+mean(p51table5.1$Actual)
+mean(p51table5.1$Reported)
+var(p51table5.1$Actual)*(7/8)
+var(p51table5.1$Reported)*(7/8)
+var(p51table5.1$Actual)
+var(p51table5.1$Reported)
